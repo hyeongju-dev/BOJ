a765513e6c03d8bfbdbb12e7bd0220d646670693
hyeongju-dev <xxox71@gmail.com>
2020-10-01 13:40:57 +0900

    refactor the code

--- 사회망 서비스.cpp
+++ 사회망 서비스.cpp
@@ -1,64 +1,61 @@
 // https://www.acmicpc.net/problem/2533
 // 친구 관계 트리가 주어졌을 때, 모든 개인이 새로운 아이디어를 수용하기 위하여 필요한 최소 얼리 어답터의 수를 구하는 프로그램을 작성하시오.
 
-#include <vector>
-#include <cstring>
-#include <algorithm>
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int dp[1000000][2];		// 1000000으로 해도 문제 맞게 풀 수 있는데, 왜 대개 1010101과 같이 여유를 둘까?
-vector<int> input[1000000];
-vector<int> g[1000000];
-int n;
+#define min(a,b) ((a) < (b) ? (a) : (b))
 
-void makeDfsTree(int curr, int prev) {
-	for (auto next : input[curr]) {
-		if (next ^ prev) {
-			g[curr].push_back(next);
-			makeDfsTree(next, curr);
-		}
-	}
+vector<int> input_g[1000001];
+vector<int> tree[1000001];
+vector<vector<int> > dp(1000001, vector<int>(2,-1));
+
+void getInputGraph() {
+  int n;
+  cin >> n;
+  for (int i = 0; i < n-1; ++i) {
+    int a, b;
+    cin >> a >> b;
+    input_g[a].push_back(b);
+    input_g[b].push_back(a);
+  }
 }
 
-int f(int curr, bool isEA) {
-	int &res = dp[curr][isEA];
-	if (res != -1)
-		return res;
+void makeDFSTree(int prev, int curr) {
+  for (auto next : input_g[curr]) {
+    if (prev != next) {
+      tree[curr].push_back(next);
+      makeDFSTree(curr, next);
+    }
+  }
+}
 
-	res = 0;
-	if (!isEA) {
-		for (auto next : g[curr])
-			res += f(next, true);
-	} else {
-		for (auto next : g[curr]) {
-			int t0 = f(next, false);
-			int t1 = f(next, true);
-			res += min(t0, t1);
-		}
-		res++;
-	}
+int cntMinEA(int curr, bool isEA) {
+  int& res = dp[curr][isEA];
+  if (res != -1)
+    return res;
 
-	return res;
+  res = 0;
+  if (isEA) {
+    for (auto next : tree[curr])
+      res += min(cntMinEA(next, false), cntMinEA(next, true));
+    res++;
+  } else {
+    for (auto next : tree[curr])
+      res += cntMinEA(next, true);
+  }
+  return res;
+}
+
+void cntMinEA() {
+  cout << min(cntMinEA(1, false), cntMinEA(1, true)) << endl;
 }
 
 int main() {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
+  getInputGraph();
+  makeDFSTree(0, 1);
+  cntMinEA();
 
-	cin >> n;
-	for (int i = 0; i < n-1; ++i) {
-		int a, b;
-		cin >> a >> b;
-		input[a].push_back(b);
-		input[b].push_back(a);
-	}
-
-	makeDfsTree(1, 0);
-
-	memset(dp, -1, sizeof(dp));
-	cout << min(f(1, false), f(1, true));
-
-	return 0;
+  return 0;
 }
